6dc4f81b11115e511a78fedd261bece39eb36eac
Hanh20021706 <hanhs8155@gmail.com>
2024-01-10 09:52:53 +0700

    feat: use useEffect function component

--- src/page/products/components/product-cart.tsx
+++ src/page/products/components/product-cart.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type ProductCartProps = {
   name?: string;
   price?: number;
 };
-
 const ProductCart = ({ name, price }: ProductCartProps) => {
   // khởi tạo một state
   const [count, setCount] = useState(1);
 
-  // update lại state
   const handleSetValue = () => {
     setCount(count + 1);
   };
+
+  const handleNothing = () => {
+    setCount(count);
+  };
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
   return (
     <>
-      {/* hiển thị state  */}
       <h2>{count}</h2>
       <button onClick={handleSetValue}>update count</button>
+      <button onClick={handleNothing}>Nothing count</button>
     </>
   );
 };
