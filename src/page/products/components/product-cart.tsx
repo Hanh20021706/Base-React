337e4c1072ebc7ef98bf9967e83bb16e9888f1f2
Hanh20021706 <hanhs8155@gmail.com>
2024-01-10 10:34:34 +0700

    feat: use useReducer function component

--- src/page/products/components/product-cart.tsx
+++ src/page/products/components/product-cart.tsx
@@ -1,30 +1,40 @@
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 
 type ProductCartProps = {
   name?: string;
   price?: number;
 };
+
+// Khai báo initialState và action type
+const initialState = { count: 0 };
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+//  Tạo reducer function
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+// Sử dụng ngoài UI component
 const ProductCart = ({ name, price }: ProductCartProps) => {
   // khởi tạo một state
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useReducer(reducer, initialState);
 
-  const handleSetValue = () => {
-    setCount(count + 1);
-  };
-
-  const handleNothing = () => {
-    setCount(count);
-  };
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
+  const handleIncrement = () => setCount({ type: INCREMENT });
+  const handleDecrement = () => setCount({ type: DECREMENT });
 
   return (
     <>
       <h2>{count}</h2>
-      <button onClick={handleSetValue}>update count</button>
-      <button onClick={handleNothing}>Nothing count</button>
+      <button onClick={handleIncrement}>increment</button>
+      <button onClick={handleDecrement}>decrement</button>
     </>
   );
 };
